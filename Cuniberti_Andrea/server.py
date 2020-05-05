6a4e7c84f159ad2d671c38a6b83dce90d8775552
AndreaCuni <48799873+AndreaCuni@users.noreply.github.com>
2020-05-05 08:37:13 +0200

    UDP socket calcolatore

---
+++ Cuniberti_Andrea/server.py
@@ -0,0 +1,27 @@
+import socket
+IpAddress = 'localhost'
+port = 5004
+
+srv = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+srv.bind((IpAddress,port))
+
+while(True):
+    #invio del messaggio
+
+
+    #ricezione del messaggio
+    data, address = srv.recvfrom(8036)
+    print(f"msg from client: {data}")
+    eval(data)
+
+    #si può interrompere il collegamento digitando '$stop'
+    if str(data,encoding="ascii") == "$stop":
+        break
+    
+    #invio messaggio di conferma
+    if data:
+        sent = srv.sendto(data, address)
+        print (f"sent {data} bytes back to {IpAddress}")
+
+srv.close()
